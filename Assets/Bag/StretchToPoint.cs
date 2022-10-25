ddacb9a292b5df51274ee6779c54f3455e8b268a
Trent <trentthebaker@gmail.com>
2022-10-24 23:57:07 -0300

    Bag handle is stretchy and exists now

---
+++ Assets/Bag/StretchToPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Bag
+{
+    public class StretchToPoint : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private float scaleScale = 1f;
+
+        private void Update()
+        {
+            if (target == null)
+                return;
+            
+            var targetPos = new Vector3(target.position.x, target.position.y, 0f);
+            var thisPos = new Vector3(transform.position.x, transform.position.y, 0f);
+            
+            var direction = target.position - transform.position;
+            var distance = direction.magnitude;
+            var scale = transform.localScale;
+            scale.y = distance * scaleScale;
+            transform.localScale = scale;
+            transform.up = direction.normalized;
+        }
+    }
+}
